def19b8936d4eef13fdd5256feccdc5ef405acc6
JohannBillar <jwbillar@gmail.com>
2016-11-06 12:02:12 -0800

    Refactor Movie into a stateless functional component

--- app/components/Movies.jsx
+++ app/components/Movies.jsx
@@ -1,55 +1,27 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import axios from 'axios';
 import MovieCard from './MovieCard';
-import APIKeys from '../../.config';
-
-const { url } = APIKeys;
 
 const propTypes = {
-  location: PropTypes.object,
-  movieSearchTerm: PropTypes.string,
+  location: PropTypes.object.isRequired,
 };
 
-class Movies extends Component {
-  constructor(props) {
-    super(props);
+function Movies(props) {
+  const params = props.location.state || {};
+  const { movies, term } = params;
+  const backButton = <Link to="/" className="back-button">Back</Link>;
 
-    this.state = {
-      movies: [],
-      pages: 1,
-    };
-  }
-
-  componentWillMount() {
-    console.log('Movies - componentWillMount invoked: ', this.props.location.state.movieSearchTerm);
-    this.fetchMoviesData(this.props.location.state.movieSearchTerm);
-  }
-
-  fetchMoviesData(movieSearch) {
-    axios.get(`${url}${movieSearch}`)
-      .then(response => this.setState({
-        movies: response.data.results,
-        pages: response.data.total_pages,
-      }))
-      .catch(error => console.log('axios get error is: ', error));
-  }
-
-  render() {
-    const backButton = <Link to="/" className="back-button">Back</Link>;
-    const header = this.props.location.state.movieSearchTerm;
-    return (
-      <div className="container">
-        {backButton}
-        <h1 className="movies-header">Results for {header}</h1>
-        <div className="movies">
-          {this.state.movies.map(movie => (
-            <MovieCard key={movie.id} {...movie} />
-          ))}
-        </div>
+  return (
+    <div className="container">
+      {backButton}
+      <h1 className="movies-header">Results for {term}</h1>
+      <div className="movies">
+        {movies.map(movie => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Movies.propTypes = propTypes;
--- app/containers/BackgroundContainer.jsx
+++ app/containers/BackgroundContainer.jsx
@@ -1,11 +1,12 @@
 import React, { PropTypes, Component } from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
+import axios from 'axios';
 import defaultImage from '../images/camera-background.jpg';
 import APIKeys from '../../.config';
 import SearchBar from '../components/SearchBar';
 import FullPageBackground from '../components/FullPageBackground';
 
-const { APP_ID, APP_SECRET, CALLBACK_URL } = APIKeys;
+const { APP_ID, APP_SECRET, CALLBACK_URL, url } = APIKeys;
 
 const unsplash = new Unsplash({
   applicationId: APP_ID,
@@ -33,6 +34,8 @@ class BackgroundContainer extends Component {
       url: '',
       windowHeight: props.height,
       inputValue: '',
+      movies: [],
+      // pages: 1,
     };
 
     this.handleWindowResize = this.handleWindowResize.bind(this);
@@ -48,6 +51,18 @@ class BackgroundContainer extends Component {
   componentDidMount() {
     if (typeof window !== 'undefined') {
       window.addEventListener('resize', this.handleWindowResize);
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.movies.length > 0) {
+      this.context.router.push({
+        pathname: '/movies',
+        state: {
+          movies: nextState.movies,
+          term: this.state.inputValue,
+        },
+      });
     }
   }
 
@@ -72,17 +87,18 @@ class BackgroundContainer extends Component {
     this.setState({ inputValue: e.target.value });
   }
 
+  fetchMoviesData(movieSearch) {
+    axios.get(`${url}${movieSearch}`)
+      .then(response => this.setState({
+        movies: response.data.results,
+        // pages: response.data.total_pages,
+      }))
+      .catch(error => console.log('axios get error is: ', error));
+  }
+
   handleFormSubmit(e) {
     e.preventDefault();
-    if (this.state.inputValue) {
-      this.removeWindowResizeEventHandler();
-      this.context.router.push({
-        pathname: '/movies',
-        state: {
-          movieSearchTerm: this.state.inputValue,
-        },
-      });
-    }
+    this.fetchMoviesData(this.state.inputValue);
   }
 
   render() {
